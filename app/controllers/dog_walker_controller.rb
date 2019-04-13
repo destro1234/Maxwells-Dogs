eb3c7ad832bc497f3f1ef467baa69f582cfdd69b
Elias Ventura <eliasventura23@gmail.com>
2019-04-13 18:02:51 +0000

    added login and show page for dogwalkers

--- app/controllers/dog_walker_controller.rb
+++ app/controllers/dog_walker_controller.rb
@@ -4,4 +4,20 @@ class DogWalkerController < ApplicationController
     erb :'/dogwalkers/signup'
   end
 
+  get '/login' do
+    erb :'/dogwalkers/login'
+  end
+
+  get '/dogwalkers/:id' do
+    @dogwalker = DogWalker.find(params[:id])
+    erb :'/dogwalkers/show'
+  end
+
+  post '/signup' do
+    @dogwalker = DogWalker.new(:name => params[:name], :username => params[:username], :password_digest => params[:password])
+    @dogwalker.save
+    session[:id] = @dogwalker.id
+    redirect "/dogwalkers/#{@dogwalker.id}"
+  end
+
 end
