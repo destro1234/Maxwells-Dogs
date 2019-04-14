47eb5bd6c6d9b4ca9cf7b9ade2506e662eac35e8
Elias Ventura <eliasventura23@gmail.com>
2019-04-14 00:17:21 +0000

    password authentication

--- app/controllers/dog_walker_controller.rb
+++ app/controllers/dog_walker_controller.rb
@@ -14,16 +14,25 @@ class DogWalkerController < ApplicationController
   end
 
   post '/signup' do
-    @dogwalker = DogWalker.new(:name => params[:name], :username => params[:username], :password_digest => params[:password])
-    @dogwalker.save
+    @dogwalker = DogWalker.new(:name => params[:name], :username => params[:username], :password => params[:password])
+
+    if @dogwalker.save
     session[:id] = @dogwalker.id
     redirect "/dogwalkers/#{@dogwalker.id}"
+    else
+      redirect '/signup'
+    end
   end
 
   post '/login' do
-    @dogwalker = DogWalker.find_by(:username => params[:username], :password_digest => params[:password])
+    @dogwalker = DogWalker.find_by(:username => params[:username])
+
+    if @dogwalker && @dogwalker.authenticate(params[:password])
     session[:id] = @dogwalker.id
     redirect "/dogwalkers/#{@dogwalker.id}"
+    else
+      redirect '/'
+    end
   end
 
   helpers do
